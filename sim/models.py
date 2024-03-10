d9c494a251389a7d122abfac2b4989f4397c9d7c
Soulless <albano.catumbela@gmail.com>
2024-03-10 20:32:17 +0100

    ...

--- sim/models.py
+++ sim/models.py
@@ -1,3 +1,11 @@
+# sim/models.py
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    photo_url = models.URLField()
+
+    def __str__(self):
+        return self.title
--- sim/views.py
+++ sim/views.py
@@ -1,3 +1,17 @@
+# sim/views.py
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+def articles(request):
+    """
+    Fetch paginated articles and render them.
+    """
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(Article.objects.all(), 10)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'articles.html', {'page_obj': page_obj})
+
+
